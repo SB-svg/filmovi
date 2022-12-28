53912222c8524d01397af230dcc76ea304ffdc9a
Snezana Bisenic <bisenic78@gmail.com>
2022-12-28 12:18:28 +0100

    Napravljena f-ja loadIndex()

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,22 @@
 const tableBody=document.getElementById('tableBody');
 
-fetch('http://localhost:3000/films')
-.then(response => response.json())
-    .then(films => {
-        films.forEach(film => {
-          tableBody.innerHTML += `
-                  <tr>
-                      <td>${film.title}</td>
-                      <td>${film.year}</td>
-                      <td>${film.rating}</td>
-                  </tr>     
-            `;
-        });
+function loadIndex(){
+    fetch('http://localhost:3000/films')
+    .then(response => response.json())
+        .then(films => {
+            films.forEach(film => {
+            tableBody.innerHTML += `
+                    <tr>
+                        <td>${film.title}</td>
+                        <td>${film.year}</td>
+                        <td>${film.rating}</td>
+                    </tr>     
+                `;
+            });
+        })
+    .catch(function(){
+        tableBody.innerText = "Error";
     });
+}
 
-    fetch();
+loadIndex();
