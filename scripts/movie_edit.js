d1cde30d9fe3da7e5b0f21d60db5d704fc04bb03
keky1330 <kcolic8@gmail.com>
2022-12-30 22:59:01 +0100

    Add movie logo input

--- scripts/movie_edit.js
+++ scripts/movie_edit.js
@@ -4,45 +4,49 @@ const movieDuration = document.getElementById('durationInput');
 const movieRating = document.getElementById('ratingInput');
 const movieDescription = document.getElementById('descriptionInput');
 const movieDirector = document.getElementById('directorInput');
+const movieLogo = document.getElementById('logoInput');
 const saveBtn = document.getElementById('saveBtn');
 
 const movieId = getQueryStringParameterByName('id');
 
 function readMovie() {
-	fetch('http://localhost:3000' + '/films/' + movieId)
-		.then(function (resp) {
-			return resp.json();
-		}).then(function (movie) {
-			movieTitle.setAttribute('value', movie.title)
-			movieProdYear.setAttribute('value', movie.year)
-			movieDuration.setAttribute('value', movie.duration)
-			movieRating.setAttribute('value', movie.rating)
-			movieDescription.setAttribute('value', movie.description)
-			movieDirector.setAttribute('value', movie.director)
-		});
+    fetch('http://localhost:3000' + '/films/' + movieId)
+        .then(function (resp) {
+            return resp.json();
+        }).then(function (movie) {
+            movieTitle.setAttribute('value', movie.title)
+            movieProdYear.setAttribute('value', movie.year)
+            movieDuration.setAttribute('value', movie.duration)
+            movieRating.setAttribute('value', movie.rating)
+            movieDescription.setAttribute('value', movie.description)
+            movieDirector.setAttribute('value', movie.director)
+            movieLogo.setAttribute('value', movie.logo)
+        });
 }
 
 readMovie();
 
 saveBtn.addEventListener('click', () => {
     // validacija
-    if (!titleInput.value ||
-        !yearInput.value ||
-        !durationInput.value ||
-        !ratingInput.value ||
-        !descriptionInput.value ||
-        !directorInput.value) {
-        alert('Morate popuniti sva polja!');
+    if (!movieTitle.value ||
+        !movieProdYear.value ||
+        !movieDuration.value ||
+        !movieRating.value ||
+        !movieDescription.value ||
+        !movieDirector.value
+    ) {
+        alert('Morate popuniti sva polja osim polja za sliku!');
         return;
     }
 
     const movie = {
-        title: titleInput.value,
-        year: parseInt(yearInput.value),
-        duration: parseInt(durationInput.value),
-        rating: parseInt(ratingInput.value),
-        description: descriptionInput.value,
-        director: directorInput.value,
+        title: movieTitle.value,
+        year: parseInt(movieProdYear.value),
+        duration: parseInt(movieDuration.value),
+        rating: parseFloat(movieRating.value),
+        description: movieDescription.value,
+        director: movieDirector.value,
+        logo: movieLogo.value,
     };
 
     let httpMethod = 'PUT';
