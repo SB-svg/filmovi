e872bce3173a7063fc96600e54f3f9b5732ae8ab
keky1330 <kcolic8@gmail.com>
2023-01-01 22:35:19 +0100

    Dodao animaciju

--- scripts/movie_delete.js
+++ scripts/movie_delete.js
@@ -6,6 +6,7 @@ const movieRating = document.getElementById('rating');
 const movieDescription = document.getElementById('description');
 const movieDirector = document.getElementById('director');
 const confirmButton = document.getElementById('confirmButton');
+const question = document.getElementById('question');
 
 const movieId = getQueryStringParameterByName('id');
 
@@ -27,17 +28,28 @@ function presentMovie() {
 presentMovie();
 
 confirmButton.addEventListener('click', function () {
+	question.innerHTML = "Brišem, molim za strpljenje";
+	confirmButton.innerHTML = '<span class="spinner-border spinner-border-sm"></span>'
+	setTimeout(() => (movieImg.parentElement.classList.add("deleteAnimation")), Math.random() * 1000)
+	setTimeout(() => (movieTitle.parentElement.classList.add("deleteAnimation")), Math.random() * 1000)
+	setTimeout(() => (movieProdYear.classList.add("deleteAnimation")), Math.random() * 1000)
+	setTimeout(() => (movieDuration.classList.add("deleteAnimation")), Math.random() * 1000)
+	setTimeout(() => (movieRating.classList.add("deleteAnimation")), Math.random() * 1000)
+	setTimeout(() => (movieDescription.classList.add("deleteAnimation")), Math.random() * 1000)
+	setTimeout(() => (movieDirector.classList.add("deleteAnimation")), Math.random() * 1000)
+	setTimeout(() => {
 
-	fetch("http://localhost:3000/films/" + movieId, {
-		method: "DELETE",
-		body: JSON.stringify({
-			id: movieId
-		}),
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json'
-		},
-	}).then(() => {
-		location.href = 'index.html';
-	})
+		fetch("http://localhost:3000/films/" + movieId, {
+			method: "DELETE",
+			body: JSON.stringify({
+				id: movieId
+			}),
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+		}).then(() => {
+			location.href = 'index.html';
+		})
+	}, 2000)
 })
